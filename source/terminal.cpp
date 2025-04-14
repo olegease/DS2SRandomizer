e71c3c7874a61b7f34b64e45f2c0ba05ac18866b
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-04-14 03:34:21 +0300

    add cmake files with wrapped rando lib and basic terminal executable

---
+++ source/terminal.cpp
@@ -0,0 +1,94 @@
+#include <modules/item_rando.hpp>
+#include <modules/randomizer.hpp>
+
+#include <iostream>
+#include <set>
+#include <string>
+
+namespace app {
+    using Commands = std::set< std::string_view >;
+    inline Commands const commands{ "enemy", "items" };
+
+    struct Args {
+        std::string_view command{ };
+        std::set< std::string_view > options{ };
+        Args( int argc, char* argv[] ) {
+            if ( argc < 2 ) return;
+            int options_from = 1;
+            if ( argv[1][0] != '-' ) {
+                if ( !commands.contains( argv[1] ) ) {
+                    std::cerr << "Unknown command: " << argv[1] << std::endl;
+                    throw std::runtime_error( "Unknown command" );
+                }
+                command = argv[1];
+                options_from = 2;
+            }
+            for ( int i = options_from; i < argc; ++i ) options.emplace( argv[i] );
+        }
+    };
+}
+
+namespace app::enemy {
+    using namespace randomizer;
+}
+
+namespace app::items {
+    using namespace item_rando;
+}
+
+int main( int argc, char *argv[] ) try {
+    std::cout << "ds2srand\n";
+
+    app::Args args{ argc, argv };
+    if ( args.options.contains( "-h" ) || args.options.contains( "--help" ) ) {
+        std::cout << "Usage: ds2srand [command] [options]\n";
+        std::cout << "Commands: (enemy and items by default\n";
+        std::cout << "\tenemy:           Enemies related options\n";
+        std::cout << "\titems:           Items related options\n";
+        std::cout << "Options:\n";
+        std::cout << "\t-h, --help       Show this help message\n";
+        std::cout << "\t-r, --restore    Restore default " << args.command << " parameters\n";
+        std::cout << std::endl;
+        return 0;
+    }
+
+    auto check_command = [&args]( std::string_view command ) {
+        return args.command.empty( ) || args.command == command;
+    };
+
+    if ( args.options.contains( "-r" ) || args.options.contains( "--restore" ) ) {
+        if ( check_command( "enemy" ) ) {
+            std::cout << "Restoring default parameters for enemies" << std::endl;
+            app::enemy::restore_default_params(false);
+        }
+
+        if ( check_command( "items" ) ) {
+            std::cout << "Restoring default parameters for items" << std::endl;
+            app::items::restore_default_params();
+        }
+
+        return EXIT_SUCCESS;
+    }
+
+    if ( check_command( "enemy" ) ) {
+        app::enemy::Data dataEnemy;
+        app::enemy::load_data( dataEnemy );
+        app::enemy::randomize( dataEnemy, false );
+        app::enemy::free_stuff( dataEnemy );
+    }
+
+    if ( check_command( "items" ) ) {
+        app::items::IRData irdata;
+        app::items::load_randomizer_data( irdata );
+        app::items::randomize_items( irdata, false );
+        app::items::free_rando_data( irdata );
+    }
+
+    return EXIT_SUCCESS;
+} catch (std::exception const &e) {
+    std::cerr << "std::exception: " << e.what( ) << std::endl;
+    return EXIT_FAILURE;
+} catch (...) {
+    std::cerr << "...: Unknown exception" << std::endl;
+    return EXIT_FAILURE;
+} // main( ) try
